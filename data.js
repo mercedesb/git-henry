d3d43f20a62593fb1bc1fae569b327a0ee112ca0
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-09-20 17:22:23 -0500

    Tudor starting point

---
+++ data.js
@@ -0,0 +1,67 @@
+const referenceData = [
+  {
+    label: "Henry VII",
+    dates: "1457 - 1509",
+    children: [
+      {
+        label: "Arthur Tudor",
+        dates: "1486 - 1502",
+      },
+      {
+        label: "Catherine of Aragon",
+        dates: "1485 - 1536",
+        married: "1509 - 1533",
+        avatar: "catherine_of_aragon.jpeg",
+        ending: "Divorced",
+        children: [
+          {
+            label: "Mary Tudor",
+            aka: "Mary I aka Bloody Mary",
+          },
+        ],
+      },
+      { label: "Henry VIII", dates: "1491 - 1547", class: "text-3xl" },
+      {
+        label: "Anne Boleyn",
+        dates: "1501 - 1536",
+        married: "1533 - 1536",
+        avatar: "anne_boleyn.jpeg",
+        ending: "Beheaded",
+        children: [{ label: "Elizabeth Tudor", aka: "Elizabeth I" }],
+      },
+      {
+        label: "Jane Seymour",
+        dates: "1508 - 1537",
+        married: "1536 - 1537",
+        avatar: "jane_seymour.jpeg",
+        ending: "Died",
+        children: [{ label: "Edward Tudor", aka: "Edward VI" }],
+      },
+      {
+        label: "Anne of Cleves",
+        dates: "1515 - 1557",
+        married: "1540 - 1540",
+        avatar: "anne_of_cleves.jpeg",
+        ending: "Divorced",
+      },
+      {
+        label: "Catherine Howard",
+        dates: "1520 - 1542",
+        married: "1540 - 1542",
+        avatar: "catherine_howard.jpeg",
+        ending: "Beheaded",
+      },
+      {
+        label: "Catherine Parr",
+        dates: "1512 - 1548",
+        married: "1543 - 1547",
+        avatar: "catherine_parr.jpeg",
+        ending: "Survived",
+      },
+    ],
+  },
+  {
+    label: "Elizabeth of York",
+    dates: "1466 - 1503",
+  },
+];
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-8 flex justify-center">
+    <family-tree :data="tudorFamily" :horizontal="false" :collapsable="true">
+    </family-tree>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tudorFamily: {
+        label: "Tudor Dynasty",
+        children: [
+          {
+            label: "Henry VII",
+            dates: "1457 - 1509",
+            children: [
+              {
+                label: "Arthur Tudor",
+                dates: "1486 - 1502",
+              },
+              {
+                label: "Catherine of Aragon",
+                dates: "1485 - 1536",
+              },
+            ],
+          },
+          {
+            label: "Elizabeth of York",
+            dates: "1466 - 1503",
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
