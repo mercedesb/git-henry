e3ffabc8e681d10361fe956e6cd1ff753baec568
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-09-20 16:16:51 -0500

    add lob dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "@/assets/main.scss";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import components from "@lob/ui-components";
+
+import "@lob/ui-components/dist/ui-components.css";
+
+const app = createApp(App);
+
+app
+  .use(components)
+  .mount("#app");
