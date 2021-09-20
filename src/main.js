e75d37afa435cc948901439ba955ab3d1197a67e
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-09-20 16:42:06 -0500

    add tree view component

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import components from "@lob/ui-components";
+import VueBlocksTree from "vue3-blocks-tree";
 
+import "vue3-blocks-tree/dist/vue3-blocks-tree.css";
 import "@lob/ui-components/dist/ui-components.css";
 
 const app = createApp(App);
 
+const defaultoptions = { treeName: "family-tree" };
+
 app
   .use(components)
+  .use(VueBlocksTree, defaultoptions)
   .mount("#app");
