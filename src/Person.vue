77f6d7ca6654fad3e5c3c85c1031d7c4fb2cd6f7
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-09-21 08:38:14 -0500

    create Person component to display extra data

---
+++ src/Person.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <strong class="text-primary-500">
+        {{ data.label }}
+      </strong>
+    </div>
+    <div class="text-sm">{{ data.dates }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Person",
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
